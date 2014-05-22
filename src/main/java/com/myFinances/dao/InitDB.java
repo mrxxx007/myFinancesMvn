14c7aeb287ea5964601ce857b5122efdceb7cb44
mrxxx007 <svpopov007@gmail.com>
2014-05-22 16:12:10 +0400

    fixed sql scripts for initBD

--- src/main/java/com/myFinances/dao/InitDB.java
+++ src/main/java/com/myFinances/dao/InitDB.java
@@ -17,22 +17,26 @@ public class InitDB {
     }
 
     public void initDB() {
+        jdbcTemplate.update("DROP TABLE IF EXISTS USER;");
         jdbcTemplate.execute("create table user (id int not null auto_increment," +
                 "    login varchar(25)," +
                 "    password VARCHAR(255)," +
                 "    primary key(id));");
 
+        jdbcTemplate.update("DROP TABLE IF EXISTS CATEGORY;");
         jdbcTemplate.execute("create table category ( " +
                 "    id int not null auto_increment, " +
                 "    name varchar(50) not null," +
-                "    user_id int," +
+                "    user_id int, primary key(id)," +
                 "    foreign key (user_id)" +
                 "    references user(id));");
 
+        jdbcTemplate.update("DROP TABLE IF EXISTS importancy;");
         jdbcTemplate.execute("create table importancy (" +
                 "    id int, " +
-                "    name varchar(50) not null);");
+                "    name varchar(50) not null, primary key(id));");
 
+        jdbcTemplate.update("DROP TABLE IF EXISTS note;");
         jdbcTemplate.execute("create table note (" +
                 "    id int not null auto_increment," +
                 "    amount double not null," +
@@ -40,13 +44,12 @@ public class InitDB {
                 "    importancy_id varchar(30)," +
                 "    notedate date not null," +
                 "    info varchar(100)," +
-                "    user_id int," +
+                "    user_id int, primary key(id)," +
                 "    foreign key (user_id)" +
                 "    references user(id)," +
                 "    foreign key (category_id)" +
                 "    references category(id)," +
                 "    foreign key (importancy_id)" +
                 "    references importancy(id));");
-
     }
 }
